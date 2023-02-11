861a9385165170781480fe0528746a7485cdf569
Ajnas N B <114915484+AjnasNB@users.noreply.github.com>
2023-02-11 07:24:50 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,19 @@
 
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+#client(bot)
+client=discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    #functions
+    general_channel= client.get_channel(your channel id)
+
+    await general_channel.send('Hello World')
+
+#run client in server
+client.run('your discord token')
